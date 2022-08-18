54692d0885dbc0752d46791d69f08ed135c847af
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2022-08-18 21:29:41 +0000

    Edge detection working (item OR player based)

--- public/Player.mjs
+++ public/Player.mjs
@@ -54,18 +54,19 @@ class Player {
         // I need to draw this.
         let iSize = Defaults.sizeCollectible;
         let pSize = Defaults.sizePlayer;
-        let iUL, iUR, iLL, iLR;
-        let pUL, pUR, pLL, pLR;
         // Oof. This all works, but I should have checked the item against the player (because item is smaller). There's a gap in the middle of the player sides that doesn't get detected.
-
-        if (this.x > item.x && this.x < item.x+iSize && 
-          this.y > item.y && this.y < item.y+iSize) return true; // UL corner
-        if (this.x+pSize > item.x && this.x+pSize<item.x+iSize &&
-          this.y > item.y && this.y < item.y+iSize) return true; // UR corner
-        if (this.x > item.x && this.x<item.x+iSize &&
-          this.y+pSize > item.y && this.y+pSize < item.y+iSize) return true; // LL corner
-        if (this.x+pSize > item.x && this.x+pSize < item.x+iSize && // LR corner
-          this.y+pSize > item.y && this.y+pSize < item.y+iSize) return true; 
+        if (pSize > iSize) {
+          if (item.x > this.x && item.x < this.x+pSize && item.y > this.y && item.y < this.y+pSize) return true; // item UL corner
+          if (item.x+iSize > this.x && item.x+iSize < this.x+pSize && item.y > this.y && item.y < this.y+pSize) return true; // item UR corner
+          if (item.x > this.x && item.x < this.x+pSize && item.y+iSize > this.y && item.y < this.y+pSize) return true; // item LL corner
+          if (item.x+iSize > this.x && item.x+iSize < this.x+pSize && item.y+iSize > this.y && item.y+iSize < this.y+pSize) return true;
+        } else {
+          // I already coded it. Who knows? Maybe the players go to giant land.  
+          if (this.x+pSize > item.x && this.x+pSize<item.x+iSize && this.y > item.y && this.y < item.y+iSize) return true; // UR corner
+          if (this.x > item.x && this.x < item.x+iSize && this.y > item.y && this.y < item.y+iSize) return true; // UL corner
+          if (this.x > item.x && this.x<item.x+iSize && this.y+pSize > item.y && this.y+pSize < item.y+iSize) return true; // LL corner
+          if (this.x+pSize > item.x && this.x+pSize < item.x+iSize && this.y+pSize > item.y && this.y+pSize < item.y+iSize) return true; // LR corner
+        } // I don't know how to make this accommodate player and item being the same size. Hope I never have to find out.
         return false
       }
     

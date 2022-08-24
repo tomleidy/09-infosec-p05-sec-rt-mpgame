64db8d9e1a71acef708ea58ebb954f733c7e79a7
Tom Leidy <1199911+tomleidy@users.noreply.github.com>
2022-08-24 18:07:26 +0000

    removed generatePlayer()

--- public/generation.mjs
+++ public/generation.mjs
@@ -16,15 +16,5 @@ const afterSlashBeforeDot = url => {
 }
 
 
-const generatePlayer = () => {
-    var xy = randXYPlayer();
-    var id = crypto.randomUUID();
-    var local =  playerList.length == 0 ? true : false
-    var playerObj = {x: xy[0], y: xy[1], score: 0, id: id, local: local};
-    var player = new Player(playerObj);
-    return player;
-} 
-
-
 
 export {randInt,afterSlashBeforeDot}
